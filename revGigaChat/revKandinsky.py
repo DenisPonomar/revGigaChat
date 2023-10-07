176c6f429d32ee0d315a5c2ddb865e39eec2f678
DenisPonomar <104255472+DenisPonomar@users.noreply.github.com>
2023-10-07 16:11:47 +0700

    Update revKandinsky.py

--- revGigaChat/revKandinsky.py
+++ revGigaChat/revKandinsky.py
@@ -35,30 +35,31 @@ def FB(prompt, width, height, negativ_prompt="", style=""):
     if response.status_code == 400 or response.status_code == 401 or response.status_code == 403:
         warnings.warn(str(json.loads(response.text)["message"]))
         return b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90wS\xde\x00\x00\x00\x01sRGB\x00\xae\xce\x1c\xe9\x00\x00\x00\x04gAMA\x00\x00\xb1\x8f\x0b\xfca\x05\x00\x00\x00\tpHYs\x00\x00\x0e\xc3\x00\x00\x0e\xc3\x01\xc7o\xa8d\x00\x00\x00\x0cIDAT\x18Wc\xf8\xff\xff?\x00\x05\xfe\x02\xfe\xa75\x81\x84\x00\x00\x00\x00IEND\xaeB`\x82'
-    time.sleep(10)
-    url = "	https://api.fusionbrain.ai/web/api/v1/text2image/status/"+json.loads(response.text)["uuid"]
-    headers = {
-                'Accept': 'application/json, text/plain, */*',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-                'Cache-Control': 'no-cache',
-                'Connection': 'keep-alive',
-                'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryrlQE4GkVXTOCFaq3',
-                'Origin': 'https://editor.fusionbrain.ai',
-                'Pragma': 'no-cache',
-                'Referer': 'https://editor.fusionbrain.ai/',
-                'Sec-Fetch-Dest': 'empty',
-                'Sec-Fetch-Mode': 'cors',
-                'Sec-Fetch-Site': 'same-origin',
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-                'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Linux"',
-    }
+    for i in range(20):
+        url = "	https://api.fusionbrain.ai/web/api/v1/text2image/status/"+json.loads(response.text)["uuid"]
+        headers = {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive',
+                    'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryrlQE4GkVXTOCFaq3',
+                    'Origin': 'https://editor.fusionbrain.ai',
+                    'Pragma': 'no-cache',
+                    'Referer': 'https://editor.fusionbrain.ai/',
+                    'Sec-Fetch-Dest': 'empty',
+                    'Sec-Fetch-Mode': 'cors',
+                    'Sec-Fetch-Site': 'same-origin',
+                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+                    'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Linux"',
+        }
 
-    # Отправляем POST-запрос
-    response = requests.get(url, headers=headers)
-    path='./out.png'
+        # Отправляем POST-запрос
+        response = requests.get(url, headers=headers)
+        if json.loads(response.text)["status"] == "DONE":
+            break
     # Выводим ответ сервера
     img = base64.b64decode(json.loads(response.text)["images"][0])
     return img
